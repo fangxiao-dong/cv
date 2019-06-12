ef6a96291767e65ae3384d69fed32c4d766ddee1
Fangxiao Dong <fangxiao.dong@outlook.com>
2019-06-11 20:24:55 -0400

    add actions for selenium page object

--- test/browser/pages/formpage.js
+++ test/browser/pages/formpage.js
@@ -6,6 +6,24 @@ class FormPage {
         this.email = driverFindByCss('input#emial');
         this.message = driverFindByCss('textarea#message');
         this.send = driverFindByCss('button.submit');
+        this.invalidEmail = driverFindByCss('span.invalid-email');
+        this.thankYouMessage = driverFindByCss('div.thankyou_message');
+    }
+
+    clickSend() {
+        this.send.click();
+    }
+
+    addInputs(elem, message) {
+        elem.sendText(message);
+    }
+
+    invalidEmailDisplayed() {
+        this.invalidEmail.isDisplayed();
+    }
+
+    thankYouMessageDisplayed() {
+        this.thankYouMessage.isDisplayed();
     }
 }
 
