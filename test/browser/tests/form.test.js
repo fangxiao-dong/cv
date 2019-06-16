ea337cce689031c20847b867f2a52af25dfc9a31
Fangxiao Dong <fangxiao.dong@outlook.com>
2019-06-16 19:46:56 -0400

    instantiate the webdriver instance and add the first test case

--- test/browser/tests/form.test.js
+++ test/browser/tests/form.test.js
@@ -1,23 +1,44 @@
 'use strict';
 
-const formPage = require('../pages/formpage');
+const FormPage = require('../pages/formpage');
 const webdriver = require('selenium-webdriver');
 const {until, By} = webdriver;
 const test = require('ava');
 
 test.before(async t => {
     t.context.driver = await (new webdriver.Builder().build());
+    const driverFindByCss = async css => await t.context.driver.findElement(By.css(css));
+    t.context.driverFindByCss = driverFindByCss;
 });
 
-test('', async t => {
+test.beforeEach(async t => {
+    await t.context.driver.get('https://127.0.0.1:8080#contact');
+    t.context.formPage = new FormPage(t.context.driverFindByCss);
+});
+
+test('Alert is present when any required inputs are empty', async t => {
+    await t.context.formPage.addInputs(t.context.formPage.message, 'This is the message from Selenium test.');
+    await t.context.formPage.clickSend();
+    try {
+        const contactAlert = await t.context.driver.wait(until.alertIsPresent());
+        t.pass("Alert is present.");
+
+        const alertText = await contactAlert.getText();
+        t.is(alertText, 'Please complete the form before submitting.');
+
+        await contactAlert.accept();
+    }
+    catch (e) {
+        console.log(e.name);
+        t.fail("Alert is not present.");
+    }
+});
+
+test('Email validation hint is present when invalid email is input', async t => {
     t.is(1,1);
 });
 
-test('', async t => {
-    t.is(1,1);
-});
-
-test('', async t => {
+test('Successfully send the contact form', async t => {
     t.is(1,1);
 });
 
